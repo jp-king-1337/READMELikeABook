523c1dff7d13a084e00c40fa7ff5c7dce4022a33
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-24 22:39:13 -0400

    created renderLicenseLink and renderLicenseSection functions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -25,13 +25,55 @@ function renderLicenseBadge(license) {
   }
 }
 
+
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return "[MIT License](https://opensource.org/licenses/MIT)";
+  } else if (license === "Apache 2.0") {
+    return "[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)";
+  } else if (license === "GNU GPL v3") {
+    return "[GNU GPL v3 License](https://www.gnu.org/licenses/gpl-3.0.en.html)";
+  } else if (license === "BSD 3-Clause") {
+    return '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+  } else if (license === "Mozilla Public License 2.0") {
+    return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+  } else if (license === "Creative Commons Zero v1.0 Universal") {
+    return '[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)';
+  } else if (license === "The Unlicense") {
+    return '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+  } else if (license === "Eclipse Public License 2.0") {
+    return '[![License](https://img.shields.io/badge/License-EPL%202.0-red.svg)](https://www.eclipse.org/legal/epl-2.0/)';
+  } else if (license === "GNU AGPL v3") {
+    return '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)';
+  } else if (license === "GNU LGPL v3") {
+    return '[![License: LGPL v3](https://img.shields.io/badge/License-LGPLv3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)';
+  } else {
+    return "";
+  }
+}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+  const licenseBadge = renderLicenseBadge(license);
+  const licenseLink = renderLicenseLink(license);
+
+  if (licenseBadge && licenseLink) {
+    return `
+## License
+
+${licenseBadge}
+
+This application is covered under the ${licenseLink}.
+`;
+  } else {
+    return "";
+  }
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
