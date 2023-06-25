4adafef3293c038def48ed8657d85233774a21f9
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-24 23:23:17 -0400

    Troubleshooting errors

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("Develop/utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -77,7 +78,12 @@ function writeToFile(sampleREADME, data) {
 
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer.createPromptModule(questions).then((answers) => {
+        const readmeContent = generateMarkdown(answers);
+        writeToFile("sampleREADME.md", markdownContent);
+    });
+}
 
 
 // Function call to initialize app
